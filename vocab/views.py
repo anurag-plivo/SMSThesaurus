f0b71903b7af7fdff27cf003c0c61af6ca084f7a
Ubuntu <ubuntu@ip-172-31-19-106.ap-south-1.compute.internal>
2018-05-04 11:34:44 +0000

    Xml Response added

--- vocab/views.py
+++ vocab/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from vocabulary.vocabulary import Vocabulary as vb
+from plivo import plivoxml
 
 import json
 import random
@@ -70,19 +71,34 @@ class VocabularyAPIView(APIView):
         return res
 
     def get(self, request, *args, **kwargs):
-        text = request.GET.get('text', '')
+        print request.GET
+        from_number = request.GET.get('From', 1)
+        to_number = request.GET.get('To', 2)
+        text = request.GET.get('Text', '')
         data = ''
         word_meaning = self.meaning(text)
         word_synonym = self.synonym(text)
         word_antonym = self.antonym(text)
-        if not word_meaning:
-            return Response(data="Word not found", status=status.HTTP_404_NOT_FOUND)
-        data += word_meaning
+        if word_meaning:
+            data += word_meaning
         if word_synonym:
             data += word_synonym
         if word_antonym:
             data += word_antonym
-        return Response(data=data, status=status.HTTP_200_OK)
+        if data=='':
+            data = 'Word not found'
+        print data
+        params = {
+            "src": to_number,
+            "dst": from_number
+        }
+        body = data
+        r = plivoxml.ResponseElement()
+        r.add_message(body, **params)
+        r.add_redirect('http://plivobin.qa.plivodev.com/1aizxag1')
+        #xmlresponse = '<Response> <Message src="' + to_number + '" dst="' + from_number + '" type="sms" callbackUrl="http://plivobin.qa.plivodev.com/1aizxag1" callbackMethod="POST">' + body + '</Message> </Response>'
+        return Response(data=r.to_string(), content_type="application/xml")
+        return Response(data=data, content_type='application/xml', status=status.HTTP_200_OK)
 
 
 
